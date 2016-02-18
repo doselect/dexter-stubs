fa9c6f0f1c57e7db43766b03e179c44778993e17
Faza Mahamood <fazamhd@gmail.com>
2016-02-18 16:01:41 +0530

    Modified android stubs
    Signed-off-by: Faza Mahamood <fazamhd@gmail.com>

--- android/app/src/main/java/com/doselect/androidapp/MainActivity.java
+++ android/app/src/main/java/com/doselect/androidapp/MainActivity.java
@@ -1,52 +1,69 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.doselect.androidapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * An {@link Activity} that gets a text string from the user and displays it back when the user
+ * clicks on one of the two buttons. The first one shows it in the same activity and the second
+ * one opens another activity and displays the message.
+ */
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    // The TextView used to display the message inside the Activity.
+    private TextView mTextView;
+
+    // The EditText where the user types the message.
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Set the listeners for the buttons.
+        findViewById(R.id.changeTextBt).setOnClickListener(this);
+        findViewById(R.id.activityChangeTextBtn).setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textToBeChanged);
+        mEditText = (EditText) findViewById(R.id.editTextUserInput);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View view) {
+        // Get the text from the EditText view.
+        final String text = mEditText.getText().toString();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (view.getId()) {
+            case R.id.changeTextBt:
+                // First button's interaction: set a text in a text view.
+                mTextView.setText(text);
+                break;
+            case R.id.activityChangeTextBtn:
+                // Second button's interaction: start an activity and send a message to it.
+                Intent intent = ShowTextActivity.newStartIntent(this, text);
+                startActivity(intent);
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
