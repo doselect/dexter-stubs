10f187389f376a18cbebab70cec029514e4d481a
saikat-sinha <saikat@myself.com>
2018-07-13 13:28:04 +0530

    Add commented snippet to handle angular routing

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -2,14 +2,27 @@ import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JsonpModule } from '@angular/http';
-import { AppComponent, EqualPipe } from './app.component';
+import { AppComponent } from './app.component';
 import { APP_BASE_HREF } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from "@angular/router";
 
 import { Http, HttpModule, RequestOptions, XHRBackend } from '@angular/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, JsonpModule, HttpModule, NgbModule.forRoot()], 
+  imports: [BrowserModule,
+            FormsModule,
+            ReactiveFormsModule,
+            JsonpModule,
+            HttpModule,
+            NgbModule.forRoot(),
+            /*
+              RouterModule.forRoot(appRoutes,{ useHash: true })
+              add this if you are using router,
+              param: appRoutes should be an array of objects
+              NOTE: using base-href will not be required
+            */
+          ],
   declarations: [ AppComponent ],
   providers: [],
   entryComponents: [],
